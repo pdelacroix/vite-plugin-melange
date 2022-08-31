e0446a3639c5bb8f54ac7c6787ccc681c6399ed4
Pierre de Lacroix <pierre@pdelacroix.com>
2022-08-31 14:14:08 +0200

    WIP working with mel watch

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import colors from 'picocolors'
 import strip from 'strip-ansi'
 import { join } from 'path';
 import { cwd } from 'process';
-import { spawnSync } from 'child_process';
-import { existsSync, promises as fsp } from 'fs';
+import { spawnSync, spawn } from 'child_process';
+import { readFileSync, existsSync, promises as fsp } from 'fs';
 
 // TODO: make configurable
 // TODO: use Vite root
@@ -39,7 +39,6 @@ function buildErrorMessage(
   args,
   includeStack = true
 ) {
-  args = [colors.red(err.message)]
   if (err.plugin) args.push(`  Plugin: ${colors.magenta(err.plugin)}`)
   if (err.id) args.push(`  File: ${colors.cyan(err.id)}`)
   if (err.frame) args.push(colors.yellow(pad(err.frame)))
@@ -54,8 +53,16 @@ function cleanStack(stack) {
     .join('\n')
 }
 
+function logWarning(server, err) {
+  server.config.logger.warn(buildErrorMessage(err, [colors.yellow(err.message)]), {
+    clear: false,
+    timestamp: true,
+    error: err
+  })
+}
+
 function logError(server, err) {
-  server.config.logger.error(buildErrorMessage(err), {
+  server.config.logger.error(buildErrorMessage(err, [colors.red(err.message)]), {
     clear: true,
     timestamp: true,
     error: err
@@ -76,14 +83,42 @@ function pad(source, n = 2) {
 
 const esyLogRE = /error: (?<message>[^]+?(?=\r?\nesy: exiting due to errors above))/
 const melangeLogRE = /File "(?<file>.+)", line (?<line>\d+), characters (?<col>[\d-]+):\r?\n(?<frame>.*)\r?\n(?<arrows>.*)\r?\n(?<message>([^]+?(?=\r?\nFile)|[^]+))/
+const melangeLogRE2 = /> File "(?<file>.+)", lines? (?<line>[\d-]+), characters (?<col>[\d-]+):\r?\n(?<frame>(> \d+[^]+?(?=\r?\n> \D))+)\r?\n(> (?<arrows>[ \^]+)\r?\n)?(?<message>(> .+\r?\n)+)/g
 
+// function compile(id) {
+//   // console.log('COMPILING for ' + id);
+//   // TODO: make configurable
+//   let { status, stderr, stdout } = spawnSync('esy', ['mel', 'build']);
+//   if (status !== 0) {
+//     throw parseError(stderr.toString());
+//   }
+// }
 function compile(id) {
-  // console.log('COMPILING for ' + id);
+  console.log('COMPILING');
   // TODO: make configurable
-  let { status, stderr, stdout } = spawnSync('esy', ['mel', 'build']);
-  if (status !== 0) {
-    throw parseError(stderr.toString());
-  }
+  // spawn('esy', ['mel', 'build', '--watch']);
+  // compiler.stdout.on('data', (data) => {
+  //   console.log(`stdout: ${data}`);
+  // });
+  //
+  // compiler.stderr.on('data', (data) => {
+  //     console.error(`stderr: ${data}`);
+  //   if (data.toString().startsWith('Waiting for filesystem changes...')) {
+  //     console.log('good')
+  //     // throw parseError(data.toString());
+  //   }
+  //   else
+  //   {
+  //     console.log('error')
+  //   }
+  // });
+  //
+  // compiler.on('close', (code) => {
+  //   console.log(`child process exited with code ${code}`);
+  // });
+  // if (status !== 0) {
+  //   throw parseError(stderr.toString());
+  // }
 }
 
 function parseError(stderr) {
@@ -108,30 +143,60 @@ function parseError(stderr) {
   }
 }
 
+function parseLog(log) {
+  // console.log(stderr);
+  let match;
+  if ((match = stderr.match(esyLogRE)) !== null) {
+    return {
+      plugin: 'melange-plugin',
+      pluginCode: "ESY_ERROR",
+      message: 'Esy error:\n' + match.groups.message
+    }
+  }
+  else if ((match = stderr.match(melangeLogRE)) !== null) {
+    return createViteError(match);
+  }
+  else {
+    return {
+      plugin: 'melange-plugin',
+      pluginCode: "UNKNOWN_ERROR",
+      message: stderr
+    }
+  }
+}
+
 function createViteError(match) {
-  const lineBorderIndex = match.groups.frame.indexOf('|') + 2;
-  const frame = '> ' + match.groups.frame + '\n' + match.groups.arrows.slice(0, lineBorderIndex) + '| ' + match.groups.arrows.slice(lineBorderIndex);
+  const lineBorderIndex = match.groups.frame.indexOf('|')
+  console.log(match.groups.frame)
+  let frame = match.groups.frame
+  if (match.groups.arrows) {
+    frame += '\n' + match.groups.arrows.slice(0, lineBorderIndex) + '| ' + match.groups.arrows.slice(lineBorderIndex);
+  }
   let file;
   if (match.groups.file.includes(build_dir)) {
     file = match.groups.file.replace(build_dir, src_dir);
   } else {
     file = join(cwd(), match.groups.file);
   }
-  let message = 'Melange compilation failed:\n' + match.groups.message
 
   return {
     plugin: 'melange-plugin',
     pluginCode: "MELANGE_COMPILATION_FAILED",
-    message: message,
+    message: match.groups.message.replace(/^> /gm, '').replace(/^Error: /, ''),
     frame: frame,
     stack: '',
     id: file,
     loc: {
       file: file,
-      line: match.groups.line,
+      line: match.groups.line.replace(/-\d+/, ''),
       column: match.groups.col,
-    }
+    },
+    isError: /^> Error: /.test(match.groups.message)
   };
+}
+
+function isMelangeLog(id) {
+  return id == join(cwd(), '_build/log');
 }
 
 function isMelangeSourceType(id) {
@@ -156,13 +221,23 @@ export default function melangePlugin() {
     name: 'melange-plugin',
     enforce: 'pre',
 
+    buildStart(options) {
+      console.log('build start')
+      console.log(join(cwd(), '_build/log'))
+        this.addWatchFile(join(cwd(), '_build/log'));
+        compile();
+      console.log(this.getWatchFiles())
+    },
+
     async resolveId(source, importer, options) {
+        // this.addWatchFile(join(cwd(), '_build/log'));
       source = cleanUrl(source);
       importer = importer && cleanUrl(importer);
       // We use the Melange entrypoint to compile for the first time
       // TODO: make entrypoint configurable
-      if (source === '/src/main.ml') {
-        compile(source);
+      if (source.endsWith('main.bs.js')) {
+        console.log('first compile ' + source)
+        // compile(source);
       }
 
       // Sometimes Melange outputs dependency compiled files
@@ -175,7 +250,9 @@ export default function melangePlugin() {
       // When a compiled file imports another compiled file,
       // `importer` will be the source file, so we resolve from the compiled file
       // and then return the source path for the resulting file
+      // console.log('resolve' + importer + ' ' + source);
       if (source.endsWith('.bs.js') && source.startsWith('.') && isMelangeSource(importer)) {
+      // console.log('resolved');
         const importerExtension = importer.slice(-3);
         importer = sourceToBuiltFile(importer);
         const resolution = await this.resolve(source, importer, { skipSelf: true, ...options });
@@ -186,16 +263,30 @@ export default function melangePlugin() {
       return null;
     },
 
+    async transform(code, id) {
+      if (id.endsWith('main.bs.js')) {
+      console.log(this.getWatchFiles())
+        // console.log('transform ' + id);
+        // this.addWatchFile(join(cwd(), '_build/log'));
+        // console.log(join(cwd(), '_build/log'));
+      // this.warn(id);
+        }
+        
+      return null;
+    },
+
     async load(id) {
       id = cleanUrl(id);
       if (isMelangeSource(id)) {
+        console.log('loading ml file')
         try {
           return await fsp.readFile(sourceToBuiltFile(id), 'utf-8')
         } catch (e) {
           // If a compiled file is imported but missing,
           // we compile again
           if (e.code === 'ENOENT') {
-            compile(id);
+            console.log('compile at load')
+            // compile(id);
           } else {
             throw e;
           }
@@ -204,12 +295,37 @@ export default function melangePlugin() {
       return null;
     },
 
-    async handleHotUpdate({ file, modules, server }) {
+    async handleHotUpdate({ file, modules, read, server }) {
+      if (isMelangeLog(file)) {
+        console.log('log changed ' + file)
+        const log = await read()
+        console.log(melangeLogRE2)
+        const matches = log.matchAll(melangeLogRE2)
+        // console.log(matches)
+        for (let match of matches) {
+          // console.log(match)
+          const err = createViteError(match);
+          console.log(err)
+
+          if (err.isError) {
+            logError(server, err)
+          } else {
+            logWarning(server, err)
+          }
+        }
+        // console.log(log)
+        // if ((match = log.matchAll(melangeLogRE2)) !== null) {
+
+        // }
+        // console.log('log: ' + log)
+      }
       // We compile when a source file has changed
       if (isMelangeSource(file)) {
         try {
-          compile(file);
+          console.log('compile at hmr')
+          // compile(file);
         } catch (err) {
+      // console.log('hot error');
           logError(server, err);
         }
       }
@@ -217,6 +333,10 @@ export default function melangePlugin() {
       // If compiled files are mistakenly watched by Vite,
       // wrong modules can be added to the graph
       return modules.filter(mod => !mod.url.endsWith('.bs.js.ml'));
+    },
+
+    watchChange(id) {
+      console.log('change ' + id);
     },
 
     configureServer(server) {
